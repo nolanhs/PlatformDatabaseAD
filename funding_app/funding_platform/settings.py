0dfdd8a8e4000e83e643ccd1cc1cf9ef79020994
alouka <160530124+alielsamra2004@users.noreply.github.com>
2025-04-24 00:08:34 +0200

    Update settings.py

--- funding_app/funding_platform/settings.py
+++ funding_app/funding_platform/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "staticfiles/admin/css"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -149,3 +152,4 @@ SIMPLE_JWT = {
 
 # Email backend for development
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
